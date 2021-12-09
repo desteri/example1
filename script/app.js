e4aa593d697f4e008e4d0a5aeef08532e6c77d3d
desteri <kyubei.dark@gmail.com>
2021-12-09 17:18:49 +0600

    added cnahges

--- script/app.js
+++ script/app.js
@@ -108,6 +108,67 @@ $(document).ready(function () {
     });
 });
 
+$('.photo__content').slick({
+    infinite: true,
+    slidesToShow: 5,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    responsive: [{
+            breakpoint: 1300,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 3000,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 1265,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 3000,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 3000,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 3000,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 3000,
+                dots: true
+            }
+        }
+    ]
+});
+
 function getNewsItem() {
     let cards = document.querySelector('.news__content ul');
     const pages = [{
@@ -277,4 +338,4 @@ function getNewsItem() {
     cards.insertAdjacentHTML('afterbegin', item);
 }
 
-getNewsItem();
+// getNewsItem();
