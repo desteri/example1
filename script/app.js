2ae2a45daaa371fdf1013bd5c4d237abf9c6bf8e
desteri <kyubei.dark@gmail.com>
2021-12-14 13:09:26 +0600

    added changes

--- script/app.js
+++ script/app.js
@@ -5,7 +5,7 @@ function navDropAndUp() {
     let up = document.querySelector('.up');
 
     window.addEventListener('scroll', () => {
-        if (document.documentElement.scrollTop > 300) {
+        if (document.documentElement.scrollTop > 100) {
             header.classList.add('fixed');
             up.style.display = 'block';
         } else {
@@ -16,21 +16,6 @@ function navDropAndUp() {
 }
 
 navDropAndUp();
-
-function getSearch() {
-    let srcPanel = document.querySelector('header .form-control');
-    let srcIcon = document.querySelectorAll('.search');
-
-    srcIcon.forEach(item => {
-        item.addEventListener('click', (e) => {
-            e.preventDefault();
-
-            srcPanel.classList.toggle('target');
-        });
-    });
-}
-
-getSearch();
 
 function getAchieve() {
     document.querySelectorAll('.achieve span').forEach(item => {
@@ -117,7 +102,7 @@ $('.photo__content').slick({
     responsive: [{
             breakpoint: 1300,
             settings: {
-                slidesToShow: 3,
+                slidesToShow: 5,
                 slidesToScroll: 1,
                 autoplay: true,
                 autoplaySpeed: 3000,
@@ -125,9 +110,9 @@ $('.photo__content').slick({
             }
         },
         {
-            breakpoint: 1265,
+            breakpoint: 1250,
             settings: {
-                slidesToShow: 5,
+                slidesToShow: 3,
                 slidesToScroll: 1,
                 autoplay: true,
                 autoplaySpeed: 3000,
@@ -137,7 +122,7 @@ $('.photo__content').slick({
         {
             breakpoint: 1024,
             settings: {
-                slidesToShow: 2,
+                slidesToShow: 3,
                 slidesToScroll: 1,
                 autoplay: true,
                 autoplaySpeed: 3000,
@@ -147,9 +132,8 @@ $('.photo__content').slick({
         {
             breakpoint: 768,
             settings: {
-                slidesToShow: 1,
+                slidesToShow: 2,
                 slidesToScroll: 1,
-                arrows: false,
                 autoplay: true,
                 autoplaySpeed: 3000,
                 dots: true
@@ -163,6 +147,66 @@ $('.photo__content').slick({
                 arrows: false,
                 autoplay: true,
                 autoplaySpeed: 3000,
+                dots: true
+            }
+        }
+    ]
+});
+
+$('.modal-body').slick({
+    infinite: true,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: true,
+    responsive: [{
+            breakpoint: 1300,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 5000,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 1250,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 5000,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 5000,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 5000,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false,
+                autoplay: true,
+                autoplaySpeed: 5000,
                 dots: true
             }
         }
@@ -220,92 +264,7 @@ function getNewsItem() {
             "src": "./img/news/9.png",
             "title": "Кыргызпатент профинансирует патентные разработки",
             "text": "Nulla non proin non diam. Aliquam morbi vestibulum, praesent condimentum. Vitae elementum pellentesque feugiat odio ornare blandit..."
-        },
-        {
-            "src": "./img/news/8.png",
-            "title": "Кыргызпатент профинансирует патентные разработки",
-            "text": "Nulla non proin non diam. Aliquam morbi vestibulum, praesent condimentum. Vitae elementum pellentesque feugiat odio ornare blandit..."
-        },
-        {
-            "src": "./img/news/7.png",
-            "title": "Кыргызпатент профинансирует патентные разработки",
-            "text": "Nulla non proin non diam. Aliquam morbi vestibulum, praesent condimentum. Vitae elementum pellentesque feugiat odio ornare blandit..."
-        },
-        {
-            "src": "./img/news/6.png",
-            "title": "Кыргызпатент профинансирует патентные разработки",
-            "text": "Nulla non proin non diam. Aliquam morbi vestibulum, praesent condimentum. Vitae elementum pellentesque feugiat odio ornare blandit..."
-        },
-        {
-            "src": "./img/news/5.png",
-            "title": "Кыргызпатент профинансирует патентные разработки",
-            "text": "Nulla non proin non diam. Aliquam morbi vestibulum, praesent condimentum. Vitae elementum pellentesque feugiat odio ornare blandit..."
-        },
-        {
-            "src": "./img/news/4.png",
-            "title": "Кыргызпатент профинансирует патентные разработки",
-            "text": "Nulla non proin non diam. Aliquam morbi vestibulum, praesent condimentum. Vitae elementum pellentesque feugiat odio ornare blandit..."
-        },
-        {
-            "src": "./img/news/3.png",
-            "title": "Кыргызпатент профинансирует патентные разработки",
-            "text": "Nulla non proin non diam. Aliquam morbi vestibulum, praesent condimentum. Vitae elementum pellentesque feugiat odio ornare blandit..."
-        },
-        {
-            "src": "./img/news/2.png",
-            "title": "Кыргызпатент профинансирует патентные разработки",
-            "text": "Nulla non proin non diam. Aliquam morbi vestibulum, praesent condimentum. Vitae elementum pellentesque feugiat odio ornare blandit..."
-        },
-        {
-            "src": "./img/news/1.png",
-            "title": "Кыргызпатент профинансирует патентные разработки",
-            "text": "Nulla non proin non diam. Aliquam morbi vestibulum, praesent condimentum. Vitae elementum pellentesque feugiat odio ornare blandit..."
-        },
-        {
-            "src": "./img/news/5.png",
-            "title": "Кыргызпатент профинансирует патентные разработки",
-            "text": "Nulla non proin non diam. Aliquam morbi vestibulum, praesent condimentum. Vitae elementum pellentesque feugiat odio ornare blandit..."
-        },
-        {
-            "src": "./img/news/7.png",
-            "title": "Кыргызпатент профинансирует патентные разработки",
-            "text": "Nulla non proin non diam. Aliquam morbi vestibulum, praesent condimentum. Vitae elementum pellentesque feugiat odio ornare blandit..."
-        },
-        {
-            "src": "./img/news/2.png",
-            "title": "Кыргызпатент профинансирует патентные разработки",
-            "text": "Nulla non proin non diam. Aliquam morbi vestibulum, praesent condimentum. Vitae elementum pellentesque feugiat odio ornare blandit..."
-        },
-        {
-            "src": "./img/news/3.png",
-            "title": "Кыргызпатент профинансирует патентные разработки",
-            "text": "Nulla non proin non diam. Aliquam morbi vestibulum, praesent condimentum. Vitae elementum pellentesque feugiat odio ornare blandit..."
-        },
-        {
-            "src": "./img/news/9.png",
-            "title": "Кыргызпатент профинансирует патентные разработки",
-            "text": "Nulla non proin non diam. Aliquam morbi vestibulum, praesent condimentum. Vitae elementum pellentesque feugiat odio ornare blandit..."
-        },
-        {
-            "src": "./img/news/8.png",
-            "title": "Кыргызпатент профинансирует патентные разработки",
-            "text": "Nulla non proin non diam. Aliquam morbi vestibulum, praesent condimentum. Vitae elementum pellentesque feugiat odio ornare blandit..."
-        },
-        {
-            "src": "./img/news/1.png",
-            "title": "Кыргызпатент профинансирует патентные разработки",
-            "text": "Nulla non proin non diam. Aliquam morbi vestibulum, praesent condimentum. Vitae elementum pellentesque feugiat odio ornare blandit..."
-        },
-        {
-            "src": "./img/news/4.png",
-            "title": "Кыргызпатент профинансирует патентные разработки",
-            "text": "Nulla non proin non diam. Aliquam morbi vestibulum, praesent condimentum. Vitae elementum pellentesque feugiat odio ornare blandit..."
-        },
-        {
-            "src": "./img/news/6.png",
-            "title": "Кыргызпатент профинансирует патентные разработки",
-            "text": "Nulla non proin non diam. Aliquam morbi vestibulum, praesent condimentum. Vitae elementum pellentesque feugiat odio ornare blandit..."
-        },
+        }
     ];
     let item = '';
 
@@ -338,4 +297,16 @@ function getNewsItem() {
     cards.insertAdjacentHTML('afterbegin', item);
 }
 
-// getNewsItem();
+getNewsItem();
+
+function tisc() {
+    let img = document.querySelectorAll('.photo__content item');
+
+    img.forEach(item => {
+        item.addEventListener('click', () => {
+
+        });
+    });
+}
+
+// tisc();
