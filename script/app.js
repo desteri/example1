dc9d35f1150354cfabe3f14f0e8b3e4157d446e0
desteri <kyubei.dark@gmail.com>
2022-01-21 17:24:06 +0300

    .

--- script/app.js
+++ script/app.js
@@ -16,3 +16,38 @@ function navDropAndUp() {
 }
 
 navDropAndUp();
+
+function isFreindly(num1, num2) {
+	let sum1 = getSum(getOwnDivisors(num1));
+	let sum2 = getSum(getOwnDivisors(num2));
+	
+	if (sum1 == num2 && sum2 == num1) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function getOwnDivisors(num) {
+    let res = [];
+    
+    for (let i = 0; i < num; i++) {
+        if (num % i == 0) {
+            res.push(i);
+        }
+    }
+
+    return res;
+}
+
+function getSum(num) {
+	let res = 0;
+    
+    for (let elem of num) {
+        res += elem;
+    }
+
+    return res;
+}
+
+console.log(isFreindly(220, 284));
